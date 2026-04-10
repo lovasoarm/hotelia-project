d3d3341c9611160b516795e9af498e55a32ac310
lovasoarm <lovasoarm@gmail.com>
2026-04-10 16:15:52 +0300

    feat: auth & userdetail service

---
+++ backend/src/main/java/com/hotelia/backend/src/main/java/com/hotelia/backend/service/AuthService.java
@@ -0,0 +1,41 @@
+package com.hotelia.backend.service;
+
+import com.hotelia.backend.dto.request.LoginRequest;
+import com.hotelia.backend.dto.response.AuthResponse;
+import com.hotelia.backend.entity.User;
+import com.hotelia.backend.exception.BadRequestException;
+import com.hotelia.backend.repository.UserRepository;
+import com.hotelia.backend.security.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final AuthenticationManager authenticationManager;
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
+
+    public AuthResponse login(LoginRequest request) {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            request.getUsername(),
+                            request.getPassword()
+                    )
+            );
+        } catch (Exception e) {
+            throw new BadRequestException("Username ou password incorrect");
+        }
+
+        User user = userRepository.findByUsername(request.getUsername())
+                .orElseThrow(() -> new BadRequestException("User non trouvé"));
+
+        String token = jwtService.generateToken(user.getUsername(), user.getRole().name());
+
+        return new AuthResponse(token, user.getUsername(), user.getRole().name());
+    }
+}

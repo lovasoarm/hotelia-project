415d3350c372bb2bafed83150871b2471c981f47
lovasoarm <lovasoarm@gmail.com>
2026-04-10 12:56:31 +0300

    create: entity for user & client

---
+++ backend/src/main/java/com/hotelia/backend/src/main/java/com/hotelia/backend/entity/Client.java
@@ -0,0 +1,34 @@
+package com.hotelia.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.List;
+
+@Entity
+@Table(name = "clients")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String phone;
+    private String address;
+
+    @OneToMany(mappedBy = "client", cascade = CascadeType.ALL)
+    private List<Reservation> reservations;
+}
---
+++ backend/src/main/java/com/hotelia/backend/src/main/java/com/hotelia/backend/entity/User.java
@@ -0,0 +1,29 @@
+package com.hotelia.backend.entity;
+
+import com.hotelia.backend.enums.Role;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+}

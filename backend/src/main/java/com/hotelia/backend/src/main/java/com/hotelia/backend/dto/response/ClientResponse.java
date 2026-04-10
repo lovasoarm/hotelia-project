2e84871661f9468a571c9a01359073932cda43e0
lovasoarm <lovasoarm@gmail.com>
2026-04-10 16:09:18 +0300

    Create: auth & client responses

---
+++ backend/src/main/java/com/hotelia/backend/src/main/java/com/hotelia/backend/dto/response/ClientResponse.java
@@ -0,0 +1,15 @@
+package com.hotelia.backend.dto.response;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ClientResponse {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String address;
+}

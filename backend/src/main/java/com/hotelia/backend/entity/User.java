5beb9e76e314e8fd22ac4aec33511de3ec3ddfba
lovasoarm <lovasoarm@gmail.com>
2026-04-13 13:18:37 +0300

    test: auth

--- backend/src/main/java/com/hotelia/backend/entity/User.java
+++ backend/src/main/java/com/hotelia/backend/entity/User.java
@@ -23,13 +23,6 @@ public class User {
 
     public User() {}
 
-    public User(Long id, String username, String password, Role role) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-        this.role = role;
-    }
-
     public Long getId() { return id; }
     public String getUsername() { return username; }
     public String getPassword() { return password; }
